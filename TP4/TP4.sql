70677de302390e9675a02422d779935f91b8db17
hayem <marc.haye@etu.umontpellier.fr>
2025-11-12 16:39:44 +0100

    add TP4

--- TP4/TP4.sql
+++ TP4/TP4.sql
@@ -1,78 +1,112 @@
-DROP TYPE employe_type FORCE;
-DROP TYPE voiture_type FORCE;
-DROP TYPE ville_type FORCE;
-DROP TYPE client_type FORCE;
-DROP TYPE projet_type FORCE;
-DROP TYPE voiture_type FORCE;
+/*
+ VILLE (idVille, nom, nbHabitants, departement)
+EMPLOYE (idEmploye, nom, prenom, salaireFixe, @ref_ville, voiture)
+PROJET (idProjet, nom, budget, @ref_chef)
+CLIENT (idClient, nom, @ref_ville, projets{@ref_projet})
+ */
 
--- 1) CREATION DES TYPES
+SELECT VALUE(v) from Ville v ;
+SELECT REF(v) FROM Ville v ;
+SELECT idClient, nom, c.ref_ville FROM Client c ;
+SELECT idClient, nom, DEREF(c.ref_ville) FROM Client c ;
 
-CREATE OR REPLACE TYPE projet_type AS OBJECT
-    (idProjet VARCHAR(5), nom VARCHAR(20), budget NUMBER, ref_chef REF employe_type,
-     MEMBER FUNCTION salaireMensuelMoyen RETURN NUMBER,
-     MEMBER PROCEDURE affecterEmploye(p_idEmploye VARCHAR, p_poste VARCHAR)
-     MEMBER FUNCTION nbProjets RETURN NUMBER
-    ) NOT FINAL;
+/*
+R1 : Le nom des projets qui ont un chef.
+NOM
+---------
+Eclipse
+JunoGallileo
+ */
 
-CREATE OR REPLACE TYPE nt_projets AS TABLE OF projet_type;
 
-CREATE OR REPLACE TYPE posseder_type AS OBJECT
-(ref_projet REF projet_type);
+/*
+R2 : Le nom et le prénom des employés qui ont une Volkswagen.
+NOM PRENOM
+-------- -------
+Ancieux Cécile
+ */
 
-CREATE OR REPLACE TYPE ville_type AS OBJECT
-    (idVille VARCHAR(5), nom VARCHAR(20), nbHabitants NUMBER, departement VARCHAR2(20),
-     MEMBER FUNCTION nbClients RETURN NUMBER,
-     MEMBER FUNCTION nbEmployes RETURN NUMBER) ;
 
-CREATE OR REPLACE TYPE voiture_type AS OBJECT
-    (immatriculation VARCHAR(20), marque VARCHAR(20));
+/*
+R3 : L'immatriculation des voitures des employés qui habitent dans le département de la
+Catalogne
+IMMAT
+------
+DD66
+ */
 
-CREATE OR REPLACE TYPE employe_type AS OBJECT
-    (idEmploye VARCHAR(5), nom VARCHAR(20), prenom VARCHAR(20), salaireFixe NUMBER, ref_ville REF ville_type, voiture voiture_type,
-     MEMBER FUNCTION nbProjets RETURN NUMBER,
-     MEMBER FUNCTION nbChef RETURN NUMBER,
-     MEMBER FUNCTION salaireMensuel RETURN NUMBER,
-     MEMBER FUNCTION estRiche RETURN NUMBER,
-     STATIC FUNCTION salaireMoyenMensuel RETURN NUMBER) NOT FINAL;
 
-CREATE OR REPLACE TYPE client_type AS OBJECT
-    (idClient VARCHAR(20), nom VARCHAR(20), ref_ville REF ville_type, projets posseder_type,
-     MEMBER FUNCTION chiffreAffaires RETURN NUMBER,
-     MEMBER FUNCTION nbProjets RETURN NUMBER);
+/*
+R4 : Pour chacun des projets, indiquer le nom du projet ainsi que le nom et le prénom du
+chef de projet.
+NOM NOMCHEF PRENOMCHEF
+--------- -------- -----------
+Eclipse Greux Nadine
+Juno Golade Larry
+Gallileo Golade Larry
+Europa
+ */
 
-CREATE OR REPLACE TYPE voiture_type AS OBJECT
-    (immatrictulation VARCHAR(5), marque VARCHAR(20));
 
--- 2) CREATION DES TABLES
+/*
+R5 : Le nom du projet pour lequel Nadine Greux est chef.
 
-CREATE TABLE VILLE OF ville_type;
-CREATE TABLE EMPLOYE OF employe_type;
-CREATE TABLE PROJET OF projet_type;
-CREATE TABLE CLIENT OF client_type;
+NOM
+--------
+Eclipse
+IUT Montpellier – Informatique – 3A 2024-2025
+BD5 — TP Dossier 2 3
+ */
 
--- 3) INSERTIONS TABLES
-INSERT INTO Ville
-VALUES (
-        ville_type('V1', 'Montpellier', 300000, 'Herault'),
-        ville_type('V2', 'Béziers', 75000, 'Herault'),
-        ville_type('V3', 'Nîmes', 5000, 'Gard'),
-        ville_type('V4', 'Perpignan', 100000, 'Catalogne')
-        );
 
-INSERT INTO Employe
-VALUES (
-        employe_type('E1','Greux','Nadine',2500),
-        employe_type('E2','Golade','Larry',3000),
-        employe_type('E3','Ancieux','Cécile',1900),
-        employe_type('E4','Zieuvert','Bruno',1800)
-        );
+/*R6 : l’identifiant, le nom, le prénom et la ville des employés qui sont chefs d'un projet qui a
+un budget de plus de 500 000 €.
+IDCHEF NOMCHEF PRENOMCHEF VILLECHEF
+------- -------- ----------- -------------
+E1 Greux Nadine Montpellier
+E2 Golade Larry Montpellier
+ */
 
-INSERT INTO Projet
-VALUES (
-        projet_type('P1','Eclipse',1000000,'E1'),
-        projet_type('P2','Juno', 800000, 'E2'),
-        projet_type('P3','Gallileo', 500000,'E2'),
-        projet_type('P4','Europa',400000,null)
-        );
-z
-SELECT VALUE(t) FROM VILLE t;
+
+/*R7 : Le nom des projets qui ont un chef montpelliérain.
+NOM
+----------
+Eclipse
+Juno
+Gallileo
+ */
+
+
+/*R8 : le nom et le prénom des employés qui habitent dans la même ville que le client IBM.
+NOM PRENOM
+------- -------
+Golade Larry
+Greux Nadine
+ */
+
+
+/*R9 : le nombre de projets de chaque client.
+NOM NB
+---------- ---
+IBM 2
+Microsoft 1
+R10 : le nom des projets du client IBM.
+NOM
+----------
+Eclipse
+Juno
+ */
+
+
+/*R11 : le nom, le prénom et la ville du chef de projet de chacun des projets qui concernent un
+client nîmois.
+NOM PRENOM VILLE
+------- ------- ------------
+Golade Larry Montpellier
+ */
+
+
+/*R12 : le nom des clients qui ont un projet dont le budget est inférieur à 600 000 €.
+NOM
+----------
+Microsoft
