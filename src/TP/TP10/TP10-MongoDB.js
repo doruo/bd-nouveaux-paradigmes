14b48d75454fd34a2726a10b6f7866f80804e9d8
hayem <marc.haye@etu.umontpellier.fr>
2026-01-08 13:56:06 +0100

    add R32 to R34

--- src/TP/TP10/TP10-MongoDB.js
+++ src/TP/TP10/TP10-MongoDB.js
@@ -23,16 +23,49 @@ db.commandes.aggregate([
 
 // R32 : Le nombre de commandes de type ‘Interne’ payées en ‘Nature’. 
 
+db.commandes.aggregate([
+    {$project: {"type": "Interne", "paiement": "Nature"}},
+    {$count: "nbCommandes"}
+]);
+
 // R33 : l’identifiant  des  commandes  passées  
 // par  le  client  Claire  Delune  qui  concernent  une Cravate,
 // classées selon l'identifiant.
 
+db.commandes.aggregate([
+    {
+        $project: 
+        {
+            "client.prenom": "Claire", 
+            "client.nom": "Delune",
+            "produits.nom": "Cravate"
+        }
+    },
+    {$sort: {"_id": 1}}
+]);
 // R34 : L’identifiant des commandes qui ont une ‘Cravate’ à 31 €.
+
+db.commandes.aggregate([
+    {
+        $project: 
+        {
+            "produits.nom": "Cravate",
+            "produits.prix": 31
+        }
+    },
+    {$sort: {"_id": 1}}
+]);
 
 // R35 : Le nombre de magasins dans chaque ville.
 
+db.commandes.aggregate([]);
+
 // R36 : Le nombre de commandes de chaque client.
+
+db.commandes.aggregate([]);
 
 // R37 : Le  montant  total  de  chaque  commande.  
 // Les  commandes  doivent  être  classées  de  la 
 // plus chère à la moins chère.
+
+db.commandes.aggregate([]);
