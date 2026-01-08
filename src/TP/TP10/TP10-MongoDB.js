78ee6c85107fb018d48702ca3aa05998ff4fd5b2
hayem <marc.haye@etu.umontpellier.fr>
2026-01-08 13:19:08 +0100

    update courses and TP names

---
+++ src/TP/TP10/TP10-MongoDB.js
@@ -0,0 +1,28 @@
+
+// Partie 1
+
+
+
+// Partie 2
+
+// Partie 3
+
+// R30 : Le nombre de commandes.
+
+// R31 : Les commandes de type ‘Interne’.
+
+// R32 : Le nombre de commandes de type ‘Interne’ payées en ‘Nature’. 
+
+// R33 : l’identifiant  des  commandes  passées  
+// par  le  client  Claire  Delune  qui  concernent  une Cravate,
+// classées selon l'identifiant.
+
+// R34 : L’identifiant des commandes qui ont une ‘Cravate’ à 31 €.
+
+// R35 : Le nombre de magasins dans chaque ville.
+
+// R36 : Le nombre de commandes de chaque client.
+
+// R37 : Le  montant  total  de  chaque  commande.  
+// Les  commandes  doivent  être  classées  de  la 
+// plus chère à la moins chère.
